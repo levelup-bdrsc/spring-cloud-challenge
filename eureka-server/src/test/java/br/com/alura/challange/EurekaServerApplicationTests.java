06778ccf9f0b12babc0003dd1443c5506b6c98dd
James Silva <jamessilva2007@gmail.com>
2022-03-09 12:11:01 -0300

    Eureka Server.

--- eureka-server/src/test/java/br/com/alura/challange/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/br/com/alura/challange/EurekaServerApplicationTests.java
@@ -1,13 +1,13 @@
 package br.com.alura.challange;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+public class EurekaServerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
