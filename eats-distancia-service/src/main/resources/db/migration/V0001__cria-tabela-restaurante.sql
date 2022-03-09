22d61323be8d6d2f98d9c89b666183460483bb26
James Silva <jamessilva2007@gmail.com>
2022-03-09 00:35:45 -0300

    WIP: eureka-server - eats-distancia-service ok

--- eats-distancia-service/src/main/resources/db/migration/V0001__cria-tabela-restaurante.sql
+++ eats-distancia-service/src/main/resources/db/migration/V0001__cria-tabela-restaurante.sql
@@ -1,5 +1,5 @@
 CREATE TABLE restaurante (
                              id bigint(20) NOT NULL AUTO_INCREMENT,
                              cep varchar(9) NOT NULL,
-                             tipo_de_cozinha_id bigint(20) NOT NULL,
+                             tipo_de_cozinha_id bigint(20) NOT NULL
 ) ;
