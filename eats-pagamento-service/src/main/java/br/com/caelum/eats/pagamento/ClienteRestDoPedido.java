c5f776e44fcac2aedd893a7cf61094de2638e611
ErikSeiki <erikseiki@gmail.com>
2022-03-10 01:37:21 -0300

    Load Feign

--- eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/ClienteRestDoPedido.java
+++ eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/ClienteRestDoPedido.java
@@ -15,20 +15,16 @@ import java.util.Map;
 @Service
 public class ClienteRestDoPedido {
 
-    private final String monolitoUrl;
-    private final RestTemplate restTemplate;
+ 
+    private final PedidoClient pedidoClient;
 
-    public ClienteRestDoPedido(@Value("${configuracao.monolito.url}") String monolitoUrl, RestTemplate restTemplate) {
-        this.monolitoUrl = monolitoUrl;
-        this.restTemplate = restTemplate;
+    public ClienteRestDoPedido(PedidoClient pedidoClient) {
+        this.pedidoClient = pedidoClient;
     }
 
     void notificaPagamentoDoPedido(Long pedidoId) {
-        String endpoint = this.monolitoUrl.concat(String.format("/pedidos/%d/status", pedidoId));
-
-        ResponseEntity<?> response = restTemplate.exchange(endpoint, HttpMethod.PUT, new HttpEntity<>(new PedidoMudancaDeStatusRequest("pago".toUpperCase())), Map.class);
-
-//        this.restTemplate.put(endpoint, new PedidoMudancaDeStatusRequest("pago".toUpperCase()));
+    	
+        ResponseEntity<?> response = pedidoClient.atualizaStatus(pedidoId, new PedidoMudancaDeStatusRequest("pago".toUpperCase()));
 
         if (!HttpStatus.valueOf(response.getStatusCodeValue()).is2xxSuccessful()) {
             throw new RuntimeException("problema ao tentar mudar o status do pedido: " + pedidoId);
---
+++ eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/PedidoClient.java
@@ -0,0 +1,15 @@
+package br.com.caelum.eats.pagamento;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient("monolito")
+public interface PedidoClient {
+	
+	@PutMapping("/pedidos/{pedidoId}/status")
+	ResponseEntity<?> atualizaStatus(@PathVariable Long pedidoId, @RequestBody PedidoMudancaDeStatusRequest pedidoParaAtualizar);
+	
+}
