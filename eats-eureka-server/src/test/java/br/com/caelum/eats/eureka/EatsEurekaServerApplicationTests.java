2a335cf3034dd7ca0b55ec41db91d4f675ebea5d
ErikSeiki <erikseiki@gmail.com>
2022-03-10 00:35:40 -0300

    Inclusão eureka

---
+++ eats-eureka-server/src/test/java/br/com/caelum/eats/eureka/EatsEurekaServerApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.caelum.eats.eureka;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EatsEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
